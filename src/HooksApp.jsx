eece478da96b63b91bfa0d3898b5cb5cbd0dfc0c
Pedro Perez <pperezi@bch.bancodechile.cl>
2022-07-01 16:35:24 -0400

    avance

---
+++ src/HooksApp.jsx
@@ -0,0 +1,5 @@
+export const HooksApp = () => {
+    return (
+        <h1>Hola Hooks App</h1>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HooksApp } from './HooksApp';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HooksApp />
   </React.StrictMode>
 )
