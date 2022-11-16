14b137eda85c0be026bd99fe00d0f49f055ec550
Pedro Perez <pperezi@bch.bancodechile.cl>
2022-11-16 17:26:09 -0300

    avances counter hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HooksApp } from './HooksApp';
+import { CounterApp } from './01-useState/CounterApp';
+// import { HooksApp } from './HooksApp';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+let dataVariable = 'demo';
+
+const funcion = () => {
+	console.log(numeros);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp />
+    <CounterApp />
+    {/* <HooksApp /> */}
   </React.StrictMode>
 )
